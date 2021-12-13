04b3e6e3b49b9449b9de3db4db70c67d7816ab8a
gurhankuras <gurhankuras@hotmail.com>
2021-12-13 16:02:18 +0300

    fix the endpoint that is not working

--- src/controllers/addHospital.controller.ts
+++ src/controllers/addHospital.controller.ts
@@ -2,7 +2,6 @@
 import {Request, Response} from 'express'
 import db from '../db/db'
 
-// query mysql'de calisirken burada sql syntax hatasi diyor
 export default async function addHospital(req: Request, res: Response) {
     console.log("ADD OFFER RUN")
     const hospitalName = req.body.hospitalName
@@ -27,34 +26,26 @@ export default async function addHospital(req: Request, res: Response) {
 
     
     const sqlStr = `
-        START TRANSACTION;
-        INSERT INTO Hospitals (hospital_name) VALUES ('${hospitalName}');
-        SET @insertedHospitalId = LAST_INSERT_ID();
-        INSERT INTO CompanyHospital (hospital_id, company_id) VALUES 
-        (@insertedHospitalId, 3);
-        COMMIT;
-        
-        `
-        //${companiesQueryStr};
-
+                START TRANSACTION;
+                INSERT INTO Hospitals (\`hospital_name\`) VALUES ('${hospitalName}');
+                SET @insertedHospitalId = LAST_INSERT_ID();
+                INSERT INTO CompanyHospital (hospital_id, company_id) VALUES 
+                ${companiesQueryStr};
+                COMMIT;
+    `
+            
     console.log(sqlStr)
     
-
     db.query(sqlStr, (err, result) => {
         if (err) {
             console.log(err)
             return res.status(400).send({message: "Bir hata oldu"})
         }
-
+        console.log(result)
         // @ts-ignore
-        if (result.affectedRows == 1) {
-            console.log(result)
-            return res.send({message: "Hastane basarili bir sekilde eklendi"})
+        if (result[1].affectedRows == 1 && result[3].affectedRows == companyIds.length) {
+            return res.status(200).send({message: "Hastane basarili bir sekilde eklendi"})
         }
-        
-        else {
-            return res.status(400).send({message: "Bir hata oldu"})
-        }
-
+        return res.status(400).send({message: "Bir hata oldu"})
     })                                                     
 }
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,6 +1,7 @@
 import mysql from 'mysql2'
 
 const db = mysql.createConnection({
+    multipleStatements: true,
     host: 'localhost',
     user: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '214g7028091',
